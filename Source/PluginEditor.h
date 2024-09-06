8dd657aeb62b90fddc9330db3c3c6096575c2fdb
gmb <gb1024@live.com>
2024-09-06 00:42:30 -0400

    public monochain

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -58,6 +58,8 @@ private:
                hiCutSlopeSliderAttachment;
     
     std::vector<juce::Component*> getComps();
+    
+    MonoChain monoChain;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ThelassicAudioProcessorEditor)
 };
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -27,6 +27,17 @@ struct ChainSettings
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
 
+using Filter = juce::dsp::IIR::Filter<float>;
+using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+enum ChainPositions
+{
+    LoCut,
+    Mid,
+    HiCut
+};
+
 //==============================================================================
 
 class ThelassicAudioProcessor  : public juce::AudioProcessor
@@ -74,18 +85,7 @@ public:
     
 
 private:
-    using Filter = juce::dsp::IIR::Filter<float>;
-    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
-    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
-
     MonoChain leftChain, rightChain;
-    
-    enum ChainPositions
-    {
-        LoCut,
-        Mid,
-        HiCut
-    };
     
     void updatePeakFilter(const ChainSettings& chainSettings);
     using Coefficients = Filter::CoefficientsPtr;
